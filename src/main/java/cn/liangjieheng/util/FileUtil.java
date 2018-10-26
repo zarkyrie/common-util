f7e26b35ade6157d10e665cebbde03c8a781497a
liangjieheng <liangjieheng@himobi.net>
2018-10-26 10:21:25 +0800

    no message

---
+++ src/main/java/cn/liangjieheng/util/FileUtil.java
@@ -0,0 +1,15 @@
+package cn.liangjieheng.util;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+
+/**
+ * 上传下载文件工具类
+ */
+public class FileUtil {
+
+    public void download(File file, HttpServletResponse httpServletResponse) {
+
+    }
+
+}
